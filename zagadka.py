63f2f1cc31f00f2b393eade738921ac00f22b62b
superigor26 <is_2001@mail.ru>
2014-09-26 20:37:40 +0400

    Create zagadka.py

---
+++ zagadka.py
@@ -0,0 +1,13 @@
+def main():
+    a=52
+    while True:
+        b=input()
+        if b<a:
+            print "malo"
+        elif b>a:
+            print "mnogo"
+        else:
+            print "pravilno"
+            input()
+            break
+main()
